b51aa5b220c62511b3014b2c1fc3df4d922dd6b5
nestor.huallpa <huallpa.nestor@gmail.com>
2026-04-24 17:26:13 -0300

    update provider

--- 01_FirstInstance/main.tf
+++ 01_FirstInstance/main.tf
@@ -1,9 +1,24 @@
 terraform {
- required_version = ">= 1.0.0"
+ required_version = ">= 1.5.0"
+ required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 4.0"
+   }
+ }
 }
 
 provider "aws" {
     region = "us-east-1"
+}
+
+terraform {
+    backend "s3" {
+        bucket = "my-terraform-state--use1-az4--x-s3"
+        key    = "my-terraform-state--use1-az4--x-s3/backup/terraform.tfstate"
+        region = "us-east-1"
+        encrypt = true
+    }
 }
 
 module "ec2_instance" {
@@ -19,12 +34,10 @@ output "public_ip" {
 
 # Output the instance ID
 output "instance_id" {
-    description = "The ID of the instance"
     value = module.ec2_instance.instance_id
 }   
 
 # Output availability zone
 output "availability_zone" {
-    description = "The availability zone of the instance"
     value = module.ec2_instance.availability_zone
 }
